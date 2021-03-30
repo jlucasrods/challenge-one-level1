dc1c9ce552c40327026cf12c2c45fd06c1706c24
jlucasrods <jlucasrods@gmail.com>
2021-03-30 15:24:19 -0300

    add test script

--- app/main.py
+++ app/main.py
@@ -7,13 +7,12 @@ from app.config.router import api_router
 from app.config.models import create_all_models
 from app.routers.pages import pages_router
 
-create_all_models()
-
 app = FastAPI(title='Challenge', version='0.1.0')
 
 app.include_router(router=api_router, prefix='/api')
 
 if not PYTEST_RUNNING:
+    create_all_models()
     app.mount('/static', StaticFiles(directory='../static'), name='static')
     app.mount('/', pages_router, name='pages')
 
